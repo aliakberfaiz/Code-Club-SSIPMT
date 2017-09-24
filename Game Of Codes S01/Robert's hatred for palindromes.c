7ebc0d34f04c58ad04d50e32bb8d09c1a20f88f0
Ali Akber Faiz <aliakbar.faiz@gmail.com>
2017-09-24 19:34:33 +0530

    trimmed unwanted code

--- Game Of Codes S01/Robert's hatred for palindromes.c
+++ Game Of Codes S01/Robert's hatred for palindromes.c
@@ -4,12 +4,9 @@ int main()
    char S[100000];
    int i,n,maxLength;
    scanf("%s",S);
-
    n=strlen(S);
    maxLength=n;
-for(i=0;S[0]==S[maxLength-1];i++){
-	maxLength--;      
-}
+for(;S[0]==S[maxLength-1];maxLength--);
     printf("%d",maxLength);
     return 0;
 }
